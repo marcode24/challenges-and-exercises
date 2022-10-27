7ea4fb3f5be2ee5de92159485faf3d97055f4b83
marco cruz <marco24cruz08@gmail.com>
2022-10-26 19:51:18 -0500

    :sparkles: multiples of 3 or 5 solution added

---
+++ javascript/multiples-of-3-or-5/multiples-of-3-or-5.js
@@ -0,0 +1,12 @@
+const solution = (number) => {
+  if (number <= 0) return 0;
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+console.log(solution(10)); // -> 23
