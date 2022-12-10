45159e51c3678e8464a28fbdea805026e4554b3e
marco cruz <marco24cruz08@gmail.com>
2022-12-09 23:38:19 -0600

    :sparkles: challenge-9 solution added

---
+++ adventjs/09-locas-luces/index.js
@@ -0,0 +1,27 @@
+function countTime(leds) {
+  leds = leds.join('').split('1');
+  leds[0] += leds.splice(-1);
+  return leds.sort((a, b) => b.length - a.length)[0].length * 7;
+}
+
+const leds = [0, 1, 1, 0, 1]
+console.log(countTime(leds)); // -> 7
+// 7 segundos ya que en el primer cambio
+// todas las luces se encendieron
+// 0s: [0, 1, 1, 0, 1]
+// 7s: [1, 1, 1, 1, 1]
+
+console.log(countTime([0, 0, 0, 1])); // -> 21
+// 21 segundos ya que necesita tres cambios:
+// 0s: [0, 0, 0, 1]
+// 7s: [1, 0, 0, 1]
+// 14s: [1, 1, 0, 1]
+// 21s: [1, 1, 1, 1]
+
+console.log(countTime([0, 0, 1, 0, 0])); // -> 28
+// 28 segundos ya que necesita cuatro cambios:
+// 0s: [0, 0, 1, 0, 0]
+// 7s: [0, 0, 1, 1, 0]
+// 14s: [0, 0, 1, 1, 1]
+// 21s: [1, 0, 1, 1, 1]
+// 28s: [1, 1, 1, 1, 1]
