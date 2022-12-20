805040457960b84d744df9f456332286fb15195c
marco cruz <marco24cruz08@gmail.com>
2022-12-20 17:10:24 -0600

    :rotating_light: eslint modified

--- adventjs/.eslintrc.js
+++ adventjs/.eslintrc.js
@@ -11,8 +11,10 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'linebreak-style': 'off',
     'no-plusplus': 'off',
     'no-param-reassign': 'off',
     'no-unused-expressions': 'off',
+    'no-return-assign': 'off',
   },
 };
